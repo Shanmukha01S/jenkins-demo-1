9c872200d98c9e3720485f440acbf917123d8b70
Shanmukha RD <shanmukhas520@email.com>
2026-01-28 22:50:20 +0530

    Added jenkins build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "====================== jenkins build ======================"
+date
+
+echo "user running : " 
+whoami
+
+echo "echo working directory : "
+pwd
+
+echo "listing files : "
+ls -l
+
+echo "====================== Build finished ================="
